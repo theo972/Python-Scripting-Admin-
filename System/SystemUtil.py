3148849442d6ba3c61ebd275fe5c8730037402b0
Theovady Moutty <theomoutty@gmail.com>
2021-03-23 16:47:43 +0100

    recuperation info system

---
+++ System/SystemUtil.py
@@ -0,0 +1,37 @@
+import psutil
+
+
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+def cpu_info():
+    cpu = {'cpu_time': psutil.cpu_times(),
+           'cpu_precent': psutil.cpu_percent(interval=1),
+           'cpu_stats': psutil.cpu_stats()}
+    return cpu
+
+
+def memory_info():
+    memory = {'virtuel_memory': psutil.virtual_memory(),
+              'swap_memory': psutil.swap_memory()}
+    return memory
+
+
+def disk_info():
+    disk = {'disk_partition': psutil.disk_partitions(all=False),
+            'disk_usage': psutil.disk_usage('/'),
+            'disk_counter': psutil.disk_io_counters()}
+    return disk
+
+
+def network_info():
+    network = {'net_counter': psutil.net_io_counters(pernic=False, nowrap=True)}
+    return network
+
+
+def sensor_info():
+    sensor = {'sensor_battery': psutil.sensors_battery()}
+    return sensor
+
--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 # Press ⌃R to execute it or replace it with your code.
 import psutil
 
-
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import System
+from System import SystemUtil
 
 
 def print_hi(name):
@@ -12,15 +13,20 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-def cpu_time():
-    cpu = psutil.cpu_times()
-    print(cpu)
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    print(cpu_time())
+    cpu_info = SystemUtil.cpu_info()
+    print(cpu_info)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    memory_info = SystemUtil.memory_info()
+    print(memory_info)
+
+    disk_info = SystemUtil.disk_info()
+    print(disk_info)
+
+    network_info = SystemUtil.network_info()
+    print(network_info)
+
+    sensor_info = SystemUtil.sensor_info()
+    print(sensor_info)
