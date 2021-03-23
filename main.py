f26f059d3e7fcc2ebddbf1007de56ae4b0d1db43
Theovady Moutty <theomoutty@gmail.com>
2021-03-23 16:15:10 +0100

    add system util

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
+import psutil
+
+
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
@@ -9,8 +12,15 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def cpu_time():
+    cpu = psutil.cpu_times()
+    print(cpu)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    print(cpu_time())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
